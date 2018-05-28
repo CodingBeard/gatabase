68eaeded2621050a6fc1ebd9cf7241597830f64b
Tim Marshall <tim@codingbeard.com>
2018-05-28 16:06:54 +0100

    Btree struct, constructor, test

--- storage/btree.go
+++ storage/btree.go
@@ -1,6 +1,17 @@
 package storage
 
-type BTree struct {
+import "io"
 
+type BTree struct {
+	Index io.ReadWriteSeeker
+	MaxElementsPerNode int8
+	Unique bool
 }
 
+func NewBTree(index io.ReadWriteSeeker, maxElementCount int8, unique bool) (BTree) {
+	return BTree{
+		Index:index,
+		MaxElementsPerNode:maxElementCount,
+		Unique:unique,
+	}
+}
--- storage/btree_test.go
+++ storage/btree_test.go
@@ -1,3 +1,24 @@
 package storage
 
+import (
+	"testing"
+	"reflect"
+)
 
+func TestNewBTree(t *testing.T) {
+	index := &MemoryFileHandle{}
+
+	btree := NewBTree(index, 4, true)
+
+	if !reflect.DeepEqual(index, btree.Index) {
+		t.Error("Index injected incorrectly")
+	}
+
+	if btree.MaxElementsPerNode != 4 {
+		t.Error("MaxElementsPerNode injected incorrectly. Expected 4, got:", btree.MaxElementsPerNode)
+	}
+
+	if btree.Unique != true {
+		t.Error("Unique injected incorrectly. Expected true, got:", btree.Unique)
+	}
+}
